70ac6ed4db751db07822a83c16c39d163604f0db
jkorr <68208126+jkorrr@users.noreply.github.com>
2022-10-16 04:48:27 -0700

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,10 +1,18 @@
+from logging import logProcesses
 from telnetlib import STATUS
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 import mediapipe as mp
-from tensorflow.keras import load_model
+from tensorflow.keras.layers import Conv1D, Dense, Dropout, Flatten, \
+    MaxPool1D, BatchNormalization, LayerNormalization, AveragePooling1D
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras import layers
+from tensorflow.keras import Sequential
+import os
+from matplotlib import pyplot as plt
+import cv2
 
 
 # data processing
@@ -14,32 +22,91 @@ mpDraw = mp.solutions.drawing_utils
 
 classes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
 
-model = load_model('mp_hand_gesture')
 loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 opt = tf.keras.optimizers.Adam(learning_rate=0.001, name = 'Adam')
-# data =  define dataset
 
-class CNN:
-    def __init__(self, input_dims):
-        self.conv1 = keras.layers.Conv2d(1, 5, 5, 1)
-        self.maxpool1 = keras.layers.MaxPool2d((2,2), stride=2)
-        self.conv2 = keras.layers.Conv2d(5, 16, 5, 1)
-        self.maxpool2 = keras.layers.MaxPool2d((2,2), stride=2)
-        self.dropout = keras.layers.Dropout(0.5)
-        self.linear1 = keras.layers.Linear(1500, 120)
-        self.linear2 = keras.layers.Linear(120, 84)
-        self.linear3 = keras.layers.Linear(84, 10) 
+x_train = np.loadtxt("datasets/train_x.csv", delimiter=",")
+# y_train = np.loadtxt("train_y.csv", delimiter=",")
+y_train = list(range(24))
+y_train.remove(14)
+y_train.remove(22)
+y_train.remove(23)
+y_train = np.array(y_train)
+"""
+def model(input_dims, l2_lambda):
+    inputs = tf.keras.Input(shape=input_dims)
+    x = tf.keras.Input(shape=input_dims)
+    # x = BatchNormalization()(inputs)
+    '''
+    for _ in range(2):
+        x = Conv1D(kernel_size=11, strides=1, filters=12,
+                   use_bias=True, kernel_regularizer=l2(l2_lambda),
+                   bias_regularizer=l2(l2_lambda), padding='same',
+                   activation='relu')(x)
+    x = Flatten()(x)
+    '''
+    x = Dense(100, activation = 'sigmoid')(x)
+    x = Dense(50, activation='sigmoid')(x)
+    output = Dense(24, activation='sigmoid')(x)
+    model = tf.keras.Model(inputs, output)
+    model.compile(loss=loss,
+                  optimizer=opt,
+                  metrics=['accuracy'])
+    return model
 
-    def forward_pass(self, x):
-        x = self.conv1(x)
-        x = tf.nn.relu(x)
-        x = self.maxpool1(x)
-        x = self.conv2(x)
-        x = tf.nn.relu(x)
-        x = self.maxpool2(x)
-        x = self.linear1(x)
-        x = self.linear2(x)
-        x = self.linear3(x)
+model_test = model(input_dims=x_train.shape[0], l2_lambda=0.005)
 
-        return x
+model_test.fit(x_train, y_train, epochs=15)
+"""
 
+def model(input_dim, plot):
+
+    # data augmenting
+
+    resize_and_rescale = tf.keras.Sequential([
+    layers.Resizing()
+    ])
+
+    model = Sequential(
+        [
+            tf.keras.Input(shape=(input_dim, )),
+            Dense(30, activation="relu"),
+            Dense(27, activation="relu"),
+            Dense(24, activation="linear"),
+
+        ]
+    )
+
+    model.compile(
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        metrics=['accuracy']
+    )
+    history = model.fit(x_train, y_train, epochs=600)
+    
+    if plot:
+        plt.plot(history.history['accuracy'])
+        plt.title('model accuracy')
+        plt.xlabel('epoch')
+        plt.ylabel('accuracy')
+        plt.show()
+    return model 
+
+x = model(63, plot=True)
+
+x_test = np.loadtxt("datasets/test_x.csv", delimiter=",").reshape(-1, 63)
+print(x_test)
+
+count = 0
+for i in range(len(x_test)):
+    pred = x.predict(x_test[i].reshape(1,63))
+    pred_index = np.argmax(pred)
+    print(classes[pred_index])
+    src = f"annotated_test_images/annotated_{i}.png"
+    img = cv2.imread(src)
+    img = cv2.putText(img, classes[pred_index], (10, 50), cv2.FONT_HERSHEY_SIMPLEX,
+    2, (0,0,255),2,cv2.LINE_AA)
+    if classes[pred_index] == x_test[i]:
+        count += 1
+    cv2.imwrite(src, img)
+print(count/len(x_test))
