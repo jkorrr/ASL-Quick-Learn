d16f41df8f2b7f37f6965fa0b9612203ee7ea283
abeinabraham <63220193+abeinabraham@users.noreply.github.com>
2022-10-16 11:40:08 -0700

    Add files via upload

--- lol.py
+++ lol.py
@@ -5,11 +5,7 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 import mediapipe as mp
-from tensorflow.keras.layers import Conv1D, Dense, Dropout, Flatten, \
-    MaxPool1D, BatchNormalization, LayerNormalization, AveragePooling1D
-from tensorflow.keras.regularizers import l2
-from tensorflow.keras import layers
-from tensorflow.keras import Sequential
+import keras.layers as layers
 import os
 from matplotlib import pyplot as plt
 import cv2
@@ -20,59 +16,38 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7, static_image_mode=True)
 mpDraw = mp.solutions.drawing_utils
 
+path = r'C:/Users/Abein/Desktop/CalHacks/webpage/datasets/train_x.csv'
+
 classes = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
 
 loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 opt = tf.keras.optimizers.Adam(learning_rate=0.001, name = 'Adam')
 
-x_train = np.loadtxt("datasets/train_x.csv", delimiter=",")
+x_train = np.loadtxt(path, delimiter=",")
 # y_train = np.loadtxt("train_y.csv", delimiter=",")
 y_train = list(range(24))
-y_train.remove(14)
-y_train.remove(22)
-y_train.remove(23)
+temp_list = []
+for i in range(24):
+    temp_list.append(i)
+    temp_list.append(i)
+y_train += temp_list
+for i in range(3):
+    y_train.remove(14)
+    y_train.remove(22)
+    y_train.remove(23)
+y_train.remove(21)
+y_train.remove(21)
 y_train = np.array(y_train)
-"""
-def model(input_dims, l2_lambda):
-    inputs = tf.keras.Input(shape=input_dims)
-    x = tf.keras.Input(shape=input_dims)
-    # x = BatchNormalization()(inputs)
-    '''
-    for _ in range(2):
-        x = Conv1D(kernel_size=11, strides=1, filters=12,
-                   use_bias=True, kernel_regularizer=l2(l2_lambda),
-                   bias_regularizer=l2(l2_lambda), padding='same',
-                   activation='relu')(x)
-    x = Flatten()(x)
-    '''
-    x = Dense(100, activation = 'sigmoid')(x)
-    x = Dense(50, activation='sigmoid')(x)
-    output = Dense(24, activation='sigmoid')(x)
-    model = tf.keras.Model(inputs, output)
-    model.compile(loss=loss,
-                  optimizer=opt,
-                  metrics=['accuracy'])
-    return model
-
-model_test = model(input_dims=x_train.shape[0], l2_lambda=0.005)
-
-model_test.fit(x_train, y_train, epochs=15)
-"""
 
 def model(input_dim, plot):
 
-    # data augmenting
-
-    resize_and_rescale = tf.keras.Sequential([
-    layers.Resizing()
-    ])
-
-    model = Sequential(
+    model = keras.Sequential(
         [
             tf.keras.Input(shape=(input_dim, )),
-            Dense(30, activation="relu"),
-            Dense(27, activation="relu"),
-            Dense(24, activation="linear"),
+            layers.Dense(90, activation="relu"),
+            layers.Dense(50, activation="relu"),
+            layers.Dense(30, activation="relu"),
+            layers.Dense(24, activation="linear"),
 
         ]
     )
@@ -82,7 +57,7 @@ def model(input_dim, plot):
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
         metrics=['accuracy']
     )
-    history = model.fit(x_train, y_train, epochs=600)
+    history = model.fit(x_train, y_train, epochs=1000)
     
     if plot:
         plt.plot(history.history['accuracy'])
@@ -92,21 +67,5 @@ def model(input_dim, plot):
         plt.show()
     return model 
 
-x = model(63, plot=True)
-
-x_test = np.loadtxt("datasets/test_x.csv", delimiter=",").reshape(-1, 63)
-print(x_test)
-
-count = 0
-for i in range(len(x_test)):
-    pred = x.predict(x_test[i].reshape(1,63))
-    pred_index = np.argmax(pred)
-    print(classes[pred_index])
-    src = f"annotated_test_images/annotated_{i}.png"
-    img = cv2.imread(src)
-    img = cv2.putText(img, classes[pred_index], (10, 50), cv2.FONT_HERSHEY_SIMPLEX,
-    2, (0,0,255),2,cv2.LINE_AA)
-    if classes[pred_index] == x_test[i]:
-        count += 1
-    cv2.imwrite(src, img)
-print(count/len(x_test))
+x = model(63, plot=False)
+x.save('webpage/model')
--- omega.py
+++ omega.py
@@ -1,15 +1,18 @@
+from tkinter.tix import IMAGE
 import cv2
 import mediapipe as mp
 import os, os.path
 import numpy as np
 import sys
+from PIL import Image
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-path = r'C:\Users\jkorr\Downloads\main\train_images'
-apath = r'C:\Users\jkorr\Downloads\main\annotated_train_images\\'
+path = r'C:\Users\Abein\Desktop\CalHacks\webpage\train_image'
+apath = r'C:\Users\Abein\Desktop\CalHacks\webpage\annotated_train_images\\'
+cpath = r'C:\Users\Abein\Desktop\CalHacks\webpage\datasets\train_x.csv'
 
 adjusted_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']
 adjusted_alphabet *= 2
@@ -20,6 +23,23 @@ LANDMARK_LIST = []
 
 # Put image file names into a list
 IMAGE_FILES = []
+li = []
+for f in os.listdir(path):
+    li.append(os.path.join(path, f))
+
+for idx, image in enumerate(li):
+    image = cv2.flip(cv2.imread(image), 1)
+    if idx + 8027 == 8041:
+        continue
+    if idx + 8027 == 8048:
+        fmt = '.jpg'
+    else:
+        fmt = '.PNG'
+    for deg in np.arange(10, 30, 10):
+        z = np.array(Image.open(f'{path}/IMG_{idx+8027}' + fmt).rotate(30))
+        z = cv2.cvtColor(z, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(f'{path}/IMG_{8048+idx}' + f'_{deg}'+ '.PNG', z)
+
 for f in os.listdir(path):
     IMAGE_FILES.append(os.path.join(path, f))
 
@@ -33,33 +53,26 @@ with mp_hands.Hands(
     for idx, file in enumerate(IMAGE_FILES):
         # Read image, flip it, and convert it (BGR) to RGB
         image = cv2.flip(cv2.imread(file), 1)
+        x, y, c = image.shape
         results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-        
-        if not results.multi_hand_landmarks:
-            cv2.namedWindow("Input")
-            cv2.imshow('image window', image)
-            cv2.waitKey(0)
+        if results.multi_hand_landmarks:
+            row = [land.x for land in results.multi_hand_landmarks[0].landmark]
+            row += [land.y for land in results.multi_hand_landmarks[0].landmark]
+            row += [land.z for land in results.multi_hand_landmarks[0].landmark]
+            
+            LANDMARK_LIST.append(row)
 
-        row = [land.x for land in results.multi_hand_landmarks[0].landmark]
-        row += [land.y for land in results.multi_hand_landmarks[0].landmark]
-        row += [land.z for land in results.multi_hand_landmarks[0].landmark]
-        
-        LANDMARK_LIST.append(row)
-        print(adjusted_alphabet[idx])
-
-        annotated_image = image.copy()
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_drawing.draw_landmarks(
-                annotated_image,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style())
-        cv2.imwrite(
-            apath + 'annotated_' + str(idx) + '.png', cv2.flip(annotated_image, 1))
-        
+            annotated_image = image.copy()
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    annotated_image,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+            cv2.imwrite(
+                apath + 'annotated_' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+            
 LANDMARK_LIST = np.array(LANDMARK_LIST)
 print(LANDMARK_LIST.shape)
-np.savetxt("datasets/train_x.csv", LANDMARK_LIST, delimiter=",")
-#np.savetxt("train_y.csv", np.array(adjusted_alphabet), fmt='%s', delimiter=',')
-
+np.savetxt(cpath, LANDMARK_LIST, delimiter=",")
